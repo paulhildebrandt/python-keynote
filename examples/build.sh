b9d1d8f2b28aba78607d003b7c98ad4a8e3dfa29
Paul Hildebrandt <paul.hildebrandt@disneyanimation.com>
2013-04-24 20:59:46 -0700

    platypus file:  Build and config files use by Platypus
    These files are used by Platypus to generate a drag and dropable Mac OS
    X application.

---
+++ examples/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# This creates a drag and drop Mac OS X app using Platypus.
+#
+# Platypus is available here: http://sveinbjorn.org/platypus
+# You will need to go into the prefs and install the command line version.
+#
+
+platypus  -P Picture\ Dump.platypus  -y Picture\ Dump.app
